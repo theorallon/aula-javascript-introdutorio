8873a377dbe15e6c44eea01ccfe0b6adfe15488e
theorallon <theo_rallon@estudante.sesisenai.org.br>
2025-09-10 20:56:43 -0300

    atividade prativa 1

---
+++ exercicio-pratico-um.js
@@ -0,0 +1,8 @@
+let meuNome = "Théo Rallon";
+let minhaIdade = 16;
+let minhaCidade = "Rio de Janeiro - RJ";
+let gostaDeProgramar = true;
+
+console.log(`Perfil: ${meuNome}, ${minhaIdade} anos!`);
+console.log(`Cidade: ${minhaCidade}`);
+console.log("Gosta de programar? ", gostaDeProgramar)
