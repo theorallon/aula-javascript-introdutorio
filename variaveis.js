058c905d72b5178a29d59b274055b12eab7601ef
theorallon <theo_rallon@estudante.sesisenai.org.br>
2025-09-10 19:31:32 -0300

    Diferenças das variaveis entre potugol e javascript

---
+++ variaveis.js
@@ -0,0 +1,26 @@
+// transição de Portugol para javascript
+//Declaração de variáveis
+
+/* Em portugol:
+cadeia nome = "Théo"
+cadeia sobrenome = "Rallon"
+inteiro idade = 16
+real altura = 1.83
+logico ehHumano = verdadeiro
+
+escreva("Nome: ", nome + sobrenome)
+escreva("Idade: ", idade)
+escreva("Altura: ", altrura)
+escreva("É humano? ", ehHumano)
+*/
+
+let nome = "Théo" // cadeia nome = "Théo"
+let sobrenome = "Rallon" // cadeia sobrenome = "Rallon"
+let idade = 16 // inteiro idade = 16
+let altura = 1.83 // real altura = 1.83
+let ehHumano = true // logico ehHumano = verdadeiro
+
+console.log("Nome: ", nome + sobrenome) // escreva("Nome: ", nome + sobrenome)
+console.log("Idade: ", idade) // escreva("Idade: ", idade)
+console.log("Altura: ", altura) // escreva("Altura: ", altrura)
+console.log("É humano? ", ehHumano) // escreva("É humano? ", ehHumano)
