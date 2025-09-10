36d2ab7e18d5dd8c746b91936eb7f6c6c1eeac7d
theorallon <theo_rallon@estudante.sesisenai.org.br>
2025-09-10 20:57:19 -0300

    condicionais em javascript

---
+++ condicionais.js
@@ -0,0 +1,15 @@
+/*
+se (nota >= 7){
+escreva("Aprovado!")
+} senao {
+ escreva("Reprovado!")
+}
+*/
+
+let nota = 0;
+
+if (nota >= 7){
+    console.log("Aprovado!");
+} else {
+    console.log("Reprovado!");
+}
