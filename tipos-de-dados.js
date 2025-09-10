afa8eaa4a4f4c510e59f5604b907be7bc7e83560
theorallon <theo_rallon@estudante.sesisenai.org.br>
2025-09-10 20:55:58 -0300

    tipos de dados em javascript

---
+++ tipos-de-dados.js
@@ -0,0 +1,13 @@
+let texto = "Hello world";
+let numero = 42;
+let decimal = 3.14;
+let logico = false;
+let nulo = null;
+let indefinido;
+
+console.log(typeof texto);
+console.log(typeof numero);
+console.log(typeof decimal);
+console.log(typeof logico);
+console.log(typeof nulo);
+console.log(typeof indefinido);
